ba1c9459c9dee53379787ebffd263e59aad71b4a
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-05-21 22:50:28 +0900

    feat: 프로젝트 세팅(1)
    * 코드 베이스 세팅
    * Xcode Cloud (CI) 세팅
    * 앱아이콘 (임시) 추가

---
+++ toduck/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  toduck
+//
+//  Created by 승재 on 5/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
