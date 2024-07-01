9df20bead3358ccd350a5e25d71f45a7c0ef610a
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-07-01 23:14:31 +0900

    re

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,2 +1,6 @@
-chmod +x tuist_install.sh
-./tuist_install.sh
+#!/bin/sh
+curl https://mise.jdx.dev/install.sh | sh
+mise install # Installs the version from .mise.toml
+
+# Runs the version of Tuist indicated in the .mise.toml file
+mise x tuist generate
