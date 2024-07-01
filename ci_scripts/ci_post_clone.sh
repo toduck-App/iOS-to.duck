46d72d5f66a1150be32f1241cd4b5435febc3b8e
박효준 <gywns626@naver.com>
2024-07-02 00:11:05 +0900

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -39,9 +39,14 @@ PROJECT_DIR="${WORKSPACE_DIR}/${PROJECT_DIR_NAME}/${PROJECT_NAME}"
 echo "- Project Directory: ${PROJECT_DIR}"
 echo "------------------------------------------------------------------"
 
+echo "global 설정"
+mise use --global node@20
 echo "\n[1] > mise install and use tuist ...\n"
 mise install tuist
 mise use tuist@4.18.0
+
+echo "global tuist setting"
+mise use -g tuist
 
 # Tuist install 실행
 echo "\n[2] > Installing Tuist ...\n"
