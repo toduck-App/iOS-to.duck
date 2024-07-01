e39676e4d9971b55faee05b138161dda2de5ef30
박효준 <gywns626@naver.com>
2024-07-02 00:02:51 +0900

    Update ci_post_clone.sh

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,19 +1,60 @@
-#!/bin/sh
-# Tuist 설치 확인 및 설치
-if ! command -v tuist &> /dev/null
-then
-    echo "Tuist가 설치되어 있지 않습니다. 설치를 시작합니다."
-    curl -Ls https://install.tuist.io | bash
-    export PATH="$HOME/.tuist/bin:$PATH"
+echo "=================================================================="
+echo "Toduck Script"
+echo "=================================================================="
+# mise 설치
+if ! command -v mise &> /dev/null; then
+  echo "\n[7] > Installing mise ...\n"
+  curl https://mise.run | sh
 else
-    echo "Tuist가 이미 설치되어 있습니다."
+  echo "mise is already installed."
 fi
 
-# Tuist 업데이트 (선택 사항)
-tuist install
+# mise 활성화
+echo "\n[8] > Activating mise ...\n"
+if ! grep -q 'eval "$(~/.local/bin/mise activate zsh)"' ~/.zshrc; then
+  echo 'eval "$(~/.local/bin/mise activate zsh)"' >> ~/.zshrc
+else
+  echo "mise is already activated in ~/.zshrc"
+fi
 
-# Tuist generate 실행
-echo "Tuist generate를 실행합니다."
-tuist generate
+echo "=================================================================="
+echo "Generate Script"
+echo "=================================================================="
 
-echo "모든 작업이 완료되었습니다."
+EXECUTION_DIR=${PWD}
+echo "- Executed From: ${EXECUTION_DIR}"
+
+SCRIPT_PATH=$(readlink -f "$0")
+echo "- Script Path: ${SCRIPT_PATH}"
+
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+echo "- Script Directory: ${SCRIPT_DIR}"
+
+WORKSPACE_DIR="$(dirname "$(dirname "${SCRIPT_DIR}")")"
+echo "- Workspace Directory: ${WORKSPACE_DIR}"
+
+PROJECT_DIR_NAME="Projects"
+PROJECT_NAME="App"
+PROJECT_DIR="${WORKSPACE_DIR}/${PROJECT_DIR_NAME}/${PROJECT_NAME}"
+echo "- Project Directory: ${PROJECT_DIR}"
+echo "------------------------------------------------------------------"
+
+echo "\n[1] > mise install and use tuist ...\n"
+mise install tuist
+mise use tuist@4.18.0
+
+# Tuist install 실행
+echo "\n[2] > Installing Tuist ...\n"
+tuist install --path "${WORKSPACE_DIR}"
+
+# Tuist generate 실행 및 프로젝트 open
+echo "\n[3] > Generating Tuist ...\n"
+if [ "$1" = "--no-open" ]; then
+    TUIST_ROOT_DIR=$PWD tuist generate --no-open
+else
+    TUIST_ROOT_DIR=$PWD tuist generate
+fi
+
+echo "\n----------------------------------"
+echo "::: Generate Script Finished :::"
+echo "----------------------------------\n"
