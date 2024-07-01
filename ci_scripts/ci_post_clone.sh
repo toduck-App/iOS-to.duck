d0ab217be628da2d1264709965f960df458f0ef2
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-07-01 23:46:20 +0900

    commit1

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,4 @@ echo "install"
 curl https://mise.jdx.dev/install.sh | sh
 mise install # Installs the version from .mise.toml
 
-# Runs the version of Tuist indicated in the .mise.toml file
 mise x tuist generate
