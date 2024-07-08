9a6005b264c71ef0f708ff5e4c6f2e0cbbac043b
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-07-09 02:40:22 +0900

    feat/config

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -14,8 +14,43 @@ echo "tuist cleaning"
 tuist clean
 echo "tuist installing"
 tuist install
+
+# *.xcconfig 파일이 생성될 폴더 경로
+FOLDER_PATH="/Volumes/workspace/repository/iOS-to.duck/Configurations"
+# *.xcconfig 파일 이름
+DEBUG_CONFIG_FILENAME="Debug.xcconfig"
+RELEASE_CONFIG_FILENAME="Release.xcconfig"
+
+DEBUG_CONFIG_FILE_PATH="$FOLDER_PATH/$DEBUG_CONFIG_FILENAME"
+RELEASE_CONFIG_FILE_PATH="$FOLDER_PATH/$RELEASE_CONFIG_FILENAME"
+
+echo "Updating $DEBUG_CONFIG_FILE_PATH..."
+{
+    echo "SERVER_URL = $SERVER_URL"
+    echo "API_KEY = $API_KEY"
+    echo "CODE_SIGN_IDENTITY = Apple Development"
+    echo "CODE_SIGN_STYLE = Manual"
+    echo "DEVELOPMENT_TEAM = $DEVELOPMENT_TEAM"
+    echo "PROVISIONING_PROFILE_SPECIFIER = $PROVISIONING_PROFILE_SPECIFIER"
+    echo "PRODUCT_BUNDLE_IDENTIFIER = $PRODUCT_BUNDLE_IDENTIFIER"
+    echo "PRODUCT_NAME = toduck"
+} >> "$DEBUG_CONFIG_FILE_PATH"
+
+echo "Updating $RELEASE_CONFIG_FILE_PATH..."
+{
+    echo "SERVER_URL = $SERVER_URL"
+    echo "API_KEY = $API_KEY"
+    echo "CODE_SIGN_IDENTITY = Apple Development"
+    echo "CODE_SIGN_STYLE = Manual"
+    echo "DEVELOPMENT_TEAM = $DEVELOPMENT_TEAM"
+    echo "PROVISIONING_PROFILE_SPECIFIER = $PROVISIONING_PROFILE_SPECIFIER"
+    echo "PRODUCT_BUNDLE_IDENTIFIER = $PRODUCT_BUNDLE_IDENTIFIER"
+    echo "PRODUCT_NAME = toduck"
+} >> "$RELEASE_CONFIG_FILE_PATH"
+
 echo "tuist generating"
 tuist generate --no-binary-cache
 echo "=================================================================="
 echo "tuist setting finish"
 echo "=================================================================="
+
