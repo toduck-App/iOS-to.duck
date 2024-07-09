de1efe14d5eb27f9bd47530fab0c29698c4c75a3
박효준 <gywns626@naver.com>
2024-07-09 19:04:39 +0900

    feat: TDFont & TDLabel 완료

--- toduck/ViewController.swift
+++ toduck/ViewController.swift
@@ -8,11 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let label: TDLabel = .init(
+        frame: .zero,
+        toduckFont: TDFont.mediumHeader1,
+        alignment: .center,
+        toduckColor: TDColor.baseBlack.color,
+        labelText: "Custom TDLabel"
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = TDColor.Diary.diarySosoColor.color
-        print("Debug2")
+        view.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            label.widthAnchor.constraint(equalToConstant: 300),
+            label.heightAnchor.constraint(equalToConstant: 100)
+        ])
     }
 }
-
