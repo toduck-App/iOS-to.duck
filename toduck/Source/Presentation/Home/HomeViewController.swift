3110d1e832234f42ea985723a3ea3cc92906292b
Kyxxn <gywns626@naver.com>
2024-07-28 01:59:53 +0900

    feat: TDSegmentedController 커스텀 작성 & 홈에서 3개 연동

--- toduck/Source/Presentation/Home/HomeViewController.swift
+++ toduck/Source/Presentation/Home/HomeViewController.swift
@@ -7,11 +7,73 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+import SnapKit
+import Then
 
+class HomeViewController: UIViewController {
+    let segmentedControl = TDSegmentedController(items: ["토덕", "일정", "루틴"])
+    
+    let todoViewController = ToduckViewController()
+    let scheduleViewController = ScheduleViewController()
+    let routineViewController = RoutineViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .systemMint
+        self.view.backgroundColor = .systemBackground
+        setupSegmentedControl()
+        setupViewControllers()
+    }
+    
+    private func setupSegmentedControl() {
+        view.addSubview(segmentedControl)
+        
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+            make.leading.equalToSuperview().offset(16)
+            make.width.equalTo(180)
+            make.height.equalTo(43)
+        }
+        
+        segmentedControl.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)
+    }
+    
+    private func setupViewControllers() {
+        addChild(todoViewController)
+          addChild(scheduleViewController)
+          addChild(routineViewController)
+          
+          view.addSubview(todoViewController.view)
+          view.addSubview(scheduleViewController.view)
+          view.addSubview(routineViewController.view)
+          
+          todoViewController.didMove(toParent: self)
+          scheduleViewController.didMove(toParent: self)
+          routineViewController.didMove(toParent: self)
+          
+          todoViewController.view.snp.makeConstraints { make in
+              make.top.equalTo(segmentedControl.snp.bottom).offset(20)
+              make.leading.trailing.bottom.equalToSuperview()
+          }
+          
+          scheduleViewController.view.snp.makeConstraints { make in
+              make.top.equalTo(segmentedControl.snp.bottom).offset(20)
+              make.leading.trailing.bottom.equalToSuperview()
+          }
+          
+          routineViewController.view.snp.makeConstraints { make in
+              make.top.equalTo(segmentedControl.snp.bottom).offset(20)
+              make.leading.trailing.bottom.equalToSuperview()
+          }
+    }
+    
+    @objc private func segmentChanged() {
+        updateView()
+    }
+    
+    private func updateView() {
+        todoViewController.view.isHidden = segmentedControl.selectedSegmentIndex != 0
+        scheduleViewController.view.isHidden = segmentedControl.selectedSegmentIndex != 1
+        routineViewController.view.isHidden = segmentedControl.selectedSegmentIndex != 2
     }
 }
