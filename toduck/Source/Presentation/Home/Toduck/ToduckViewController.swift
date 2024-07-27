1528bdceb573bd673bc770d99d2fff3bf17ff7ad
Kyxxn <gywns626@naver.com>
2024-07-28 01:59:53 +0900

    feat: TDSegmentedController 커스텀 작성 & 홈에서 3개 연동

---
+++ toduck/Source/Presentation/Home/Toduck/ToduckViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ToduckViewController.swift
+//  toduck
+//
+//  Created by 박효준 on 7/28/24.
+//
+
+import UIKit
+
+final class ToduckViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
