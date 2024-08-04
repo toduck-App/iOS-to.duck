571a696ef5c48ec74b95a3d7b91669d9c9761315
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-08-04 16:02:40 +0900

    feat: add BaseView & updated to iOS version 16

---
+++ toduck/Source/Presentation/Common/BaseViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class BaseViewController<LayoutView: BaseView>: UIViewController {
+
+    var layoutView = LayoutView()
+    func layout() {}
+    func configure() {}
+    func addView() {}
+    func binding() {}
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor =  TDColor.Neutral.neutral50
+        self.configure()
+        self.addView()
+        self.layout()
+        self.binding()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //self.endEditing(true)
+    }
+    
+    override func loadView() {
+        if layoutView.backgroundColor == nil {
+            layoutView.backgroundColor = TDColor.Neutral.neutral50
+        }
+        view = layoutView
+    }
+}
