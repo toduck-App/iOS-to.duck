b1a67e9d3fa261435c0c25b28a62d5dcf52104c5
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-08-10 14:43:21 +0900

    feat: Chip, Feed 개발

---
+++ toduck/Source/Presentation/Social/Cell/SocialFeedCollectionViewCell.swift
@@ -0,0 +1,288 @@
+//
+//  SocialFeedCollectionViewCell.swift
+//  toduck
+//
+//  Created by 승재 on 8/3/24.
+//
+import SnapKit
+import Then
+import Foundation
+import Kingfisher
+import UIKit
+
+class SocialFeedCollectionViewCell: UICollectionViewCell {
+    private let containerView = UIView()
+    
+    private var verticalStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 14
+        $0.distribution = .fill
+    }
+    private var headerStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.spacing = 10
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    private var headerLeftStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .leading
+        stackView.spacing = 10
+        stackView.distribution = .equalSpacing
+        return stackView
+    }()
+    
+    
+    private var headerRightStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.alignment = .fill
+        $0.spacing = 10
+        $0.distribution = .equalSpacing
+    }
+    
+    private var bodyStackView = UIStackView().then{
+        $0.axis = .vertical
+        $0.spacing = 10
+        $0.alignment = .fill
+        $0.distribution = .fill
+    }
+    
+    private var footerView = UIView()
+    
+    lazy var dotIconView = UIImageView().then {
+        $0.tintColor = TDColor.Neutral.neutral500
+        $0.contentMode = .scaleAspectFit
+        $0.image = TDImage.Dot.horizontalMedium
+    }
+    
+    
+    
+    lazy var likeIconView = UIImageView().then {
+        $0.tintColor = TDColor.Neutral.neutral500
+        $0.contentMode = .scaleAspectFit
+        $0.image = TDImage.likeMedium
+    }
+    
+    lazy var commentIconView = UIImageView().then {
+        $0.tintColor = TDColor.Neutral.neutral500
+        $0.contentMode = .scaleAspectFit
+        $0.image = TDImage.commentSmall
+    }
+    
+    lazy var shareIconView = UIImageView().then {
+        $0.tintColor = TDColor.Neutral.neutral500
+        $0.contentMode = .scaleAspectFit
+        $0.image = TDImage.repeatSmall
+    }
+    
+    lazy var avatarView = UIImageView().then {
+        $0.contentMode = .scaleAspectFit
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 20
+        $0.backgroundColor = TDColor.Neutral.neutral100
+    }
+    
+    private var titleLabel = TDLabel(toduckFont: .mediumCaption2, toduckColor: TDColor.Primary.primary500, labelText:  "").then{
+        $0.backgroundColor = TDColor.Primary.primary50
+    }
+    private var nicknameLabel = TDLabel(toduckFont: .mediumBody2, toduckColor: TDColor.Neutral.neutral700)
+    private var dateLabel = TDLabel(toduckFont: .regularBody2, toduckColor: TDColor.Neutral.neutral500)
+    
+    private var contentLabel = TDLabel(toduckFont: .mediumBody2, toduckColor: TDColor.Neutral.neutral800).then {
+        $0.numberOfLines = 0
+    }
+    
+    private var likeLabel = TDLabel(toduckFont: .regularBody2, toduckColor: TDColor.Neutral.neutral600)
+    private var commentLabel = TDLabel(toduckFont: .regularBody2, toduckColor: TDColor.Neutral.neutral600)
+    private var shareLabel = TDLabel(toduckFont: .regularBody2, toduckColor: TDColor.Neutral.neutral600)
+    
+    private var routineView = UIView().then{
+        $0.backgroundColor = TDColor.Neutral.neutral100
+        $0.layer.cornerRadius = 8
+        $0.clipsToBounds = true
+    }
+    
+    // Semibold가 없어요
+    private var routineTitleLabel = TDLabel(toduckFont: .boldBody1, toduckColor: TDColor.Neutral.neutral800).then {
+        $0.numberOfLines = 0
+    }
+    
+    private var routineContentLabel = TDLabel(toduckFont: .regularBody2, toduckColor: TDColor.Neutral.neutral800).then{
+        $0.numberOfLines = 0
+    }
+    
+    private var routineDateLabel = TDLabel(toduckFont: .mediumBody2, toduckColor: TDColor.Neutral.neutral500)
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    func configure(with item: Post) {
+        titleLabel.setText(item.user.title)
+        nicknameLabel.setText(item.user.name)
+        dateLabel.setText("1시간 전")
+        contentLabel.setText(item.contentText)
+        guard let likeCount = item.likeCount, let commentCount = item.commentCount else { return }
+        likeLabel.setText("\(likeCount)")
+        commentLabel.setText("\(commentCount)")
+        
+        guard let url = URL(string: item.user.icon) else { return }
+        loadImages(url: url)
+        
+        if let shareCount = item.shareCount, shareCount > 0 {
+            shareLabel.setText("\(shareCount)")
+        } else {
+            shareIconView.isHidden = true
+            shareLabel.isHidden = true
+        }
+        
+        if let routine = item.routine {
+            bodyStackView.addArrangedSubview(routineView)
+            routineView.isHidden = false
+            routineTitleLabel.setText(routine.title)
+            routineContentLabel.setText(routine.memo ?? "sdf")
+            routineDateLabel.setText(routine.dateString() ?? "AM 00:00")
+            
+            setupRoutineView()
+        }
+    }
+}
+private extension SocialFeedCollectionViewCell {
+    
+    func setupUI() {
+        setupLayout()
+    }
+    
+    func loadImages(url: URL) {
+        avatarView.image = TDImage.Social.colorMedium
+    }
+    
+    func setupLayout() {
+        addSubview(containerView)
+        containerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.bottom.equalToSuperview().offset(-20)
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        containerView.addSubview(avatarView)
+        avatarView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.size.equalTo(36)
+        }
+        
+        headerLeftStackView.addArrangedSubview(titleLabel)
+        headerLeftStackView.addArrangedSubview(nicknameLabel)
+        headerLeftStackView.addArrangedSubview(dateLabel)
+        
+        headerRightStackView.addArrangedSubview(dotIconView)
+        headerStackView.addArrangedSubview(headerLeftStackView)
+        headerStackView.addArrangedSubview(headerRightStackView)
+        
+        bodyStackView.addArrangedSubview(contentLabel)
+        containerView.addSubview(verticalStackView)
+        verticalStackView.addArrangedSubview(headerStackView)
+        verticalStackView.addArrangedSubview(bodyStackView)
+        verticalStackView.addArrangedSubview(footerView)
+        
+        footerView.addSubview(likeIconView)
+        footerView.addSubview(likeLabel)
+        footerView.addSubview(commentIconView)
+        footerView.addSubview(commentLabel)
+        footerView.addSubview(shareIconView)
+        footerView.addSubview(shareLabel)
+        
+        [titleLabel, nicknameLabel, dateLabel, dotIconView, likeIconView,likeLabel,commentIconView,commentLabel,shareIconView,shareLabel].forEach {
+            $0.snp.makeConstraints { make in
+                make.centerY.equalToSuperview()
+            }
+        }
+        
+        verticalStackView.snp.makeConstraints { make in
+            make.leading.equalTo(avatarView.snp.trailing).offset(10)
+            make.trailing.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+        headerStackView.snp.makeConstraints { make in
+            make.height.equalTo(24)
+        }
+        footerView.snp.makeConstraints { make in
+            make.height.equalTo(24)
+        }
+        
+        dotIconView.snp.makeConstraints { make in
+            make.size.equalTo(24)
+        }
+        likeIconView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.size.equalTo(24)
+        }
+        
+        likeLabel.snp.makeConstraints { make in
+            make.leading.equalTo(likeIconView.snp.trailing).offset(2)
+            make.centerY.equalTo(likeIconView)
+        }
+        
+        commentIconView.snp.makeConstraints { make in
+            make.leading.equalTo(likeLabel.snp.trailing).offset(10)
+            make.size.equalTo(24)
+        }
+        
+        commentLabel.snp.makeConstraints { make in
+            make.leading.equalTo(commentIconView.snp.trailing).offset(2)
+            make.centerY.equalTo(commentIconView)
+        }
+        
+        shareIconView.snp.makeConstraints { make in
+            make.leading.equalTo(commentLabel.snp.trailing).offset(10)
+            make.size.equalTo(24)
+        }
+        
+        shareLabel.snp.makeConstraints { make in
+            make.leading.equalTo(shareIconView.snp.trailing).offset(2)
+            make.centerY.equalTo(shareIconView)
+        }
+        
+    }
+    
+    
+    func setupRoutineView() {
+        
+        bodyStackView.addArrangedSubview(routineView)
+        routineView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(114)
+        }
+        routineView.addSubview(routineTitleLabel)
+        routineView.addSubview(routineContentLabel)
+        routineView.addSubview(routineDateLabel)
+        routineTitleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(14)
+            make.leading.equalToSuperview().offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        
+        routineContentLabel.snp.makeConstraints { make in
+            make.top.equalTo(routineTitleLabel.snp.bottom).offset(10)
+            make.leading.trailing.equalTo(routineTitleLabel)
+            
+        }
+        
+        routineDateLabel.snp.makeConstraints { make in
+            make.top.equalTo(routineContentLabel.snp.bottom).offset(18)
+            make.leading.trailing.equalTo(routineTitleLabel)
+            make.bottom.equalToSuperview().offset(-10)
+        }
+    }
+}
--- toduck/Source/Presentation/Social/SocialViewController.swift
+++ toduck/Source/Presentation/Social/SocialViewController.swift
@@ -6,11 +6,113 @@
 //
 
 import UIKit
+import Then
+import SnapKit
 
 class SocialViewController: UIViewController, TDSheetPresentation {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .systemBlue
+        self.view.backgroundColor = .white
+        self.setupUI()
+        self.socialFeedCollectionView.dataSource = self
+        self.socialFeedCollectionView.delegate = self
     }
 }
+
+extension SocialViewController: TDChipCollectionViewDelegate {
+    func chipCollectionView(_ collectionView: TDChipCollectionView, didSelectChipAt index: Int, chipText: String) {
+        print("[LOG] 현재 Select 한 Chip: \(chipText) , Index = : \(index)")
+    }
+}
+
+extension SocialViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: SocialFeedCollectionViewCell = collectionView.dequeueReusableCell(for: indexPath)
+        let post = posts[indexPath.item]
+        cell.configure(with: post)
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("[LOG] Clicked")
+    }
+}
+
+private extension SocialViewController {
+    
+    private func setupUI() {
+        //MARK: - layout 싹 다 다시 잡아야함
+        view.addSubview(chipCollectionView)
+        view.addSubview(chipCollectionView2)
+        view.addSubview(socialFeedCollectionView)
+        chipCollectionView.chipDelegate = self
+        chipCollectionView2.chipDelegate = self
+        socialFeedCollectionView.register(with: SocialFeedCollectionViewCell.self)
+        chipCollectionView.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalToSuperview().offset(100)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+        }
+        chipCollectionView2.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(chipCollectionView.snp.bottom).offset(20)
+            make.leading.equalToSuperview().offset(10)
+            make.trailing.equalToSuperview().offset(-10)
+        }
+        
+        chipCollectionView.setChips(chipTexts)
+        chipCollectionView2.setChips(chipTexts)
+        
+        socialFeedCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(chipCollectionView2.snp.bottom).offset(20)
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.width.equalToSuperview()
+        }
+        
+        
+    }
+}
+
+private extension SocialViewController {
+    func makeCollectionViewLayout() -> UICollectionViewLayout {
+        let itemPadding: CGFloat = 10
+        let groupPadding: CGFloat = 16
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(300)
+        )
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .estimated(300)
+        )
+        
+        let edgeSpacing = NSCollectionLayoutEdgeSpacing(
+            leading: .fixed(0),
+            top: .fixed(itemPadding),
+            trailing: .fixed(0),
+            bottom: .fixed(itemPadding)
+        )
+        
+        let groupInset = NSDirectionalEdgeInsets(
+            top: 0,
+            leading: groupPadding,
+            bottom: 0,
+            trailing: groupPadding
+        )
+        
+        return UICollectionViewCompositionalLayout.makeVerticalCompositionalLayout(
+            itemSize: itemSize,
+            itemEdgeSpacing: edgeSpacing,
+            groupSize: groupSize,
+            groupInset: groupInset
+        )
+    }
+}
+
+
