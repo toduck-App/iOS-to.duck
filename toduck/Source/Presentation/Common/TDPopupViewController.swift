506cbd3644c0bdec0ec8e0882f131863ad7f1968
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2024-09-07 15:26:23 +0900

    feat: add PopupViewController

--- toduck/Source/Presentation/Common/TDPopupViewController.swift
+++ toduck/Source/Presentation/Common/TDPopupViewController.swift
@@ -1,71 +1,33 @@
+import SnapKit
 import UIKit
 
-class TDPopupViewController: UIViewController {
-    var popupStackView: UIStackView = UIStackView().then {
-        $0.axis = .vertical
-        $0.spacing = 24
-    }
-
-    var whiteBoard: UIView = UIView().then {
-        $0.backgroundColor = .white
-        $0.layer.cornerRadius = 28
-    }
-
-    var delegate: TDPopupPresentDelegate?
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        configure()
-        addView()
-        layout()
-    }
-
-    private func configure() {
+class TDPopupViewController<LayerView: BaseView>: BaseViewController<BaseView>, UIGestureRecognizerDelegate {
+    let layerView = LayerView()
+    override func configure() {
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(dismissPopup))
         tapGesture.delegate = self
-        view.addGestureRecognizer(tapGesture)
-        view.isUserInteractionEnabled = true
+        layoutView.addGestureRecognizer(tapGesture)
+        layoutView.isUserInteractionEnabled = true
+        layoutView.backgroundColor = .black.withAlphaComponent(0.5)
 
-        view.backgroundColor = .black.withAlphaComponent(0.5)
-
+        //layerView.backgroundColor = .white
     }
 
-    private func addView() {
-        if let titleView = delegate?.popupTitleView(popupVc: self) {
-            popupStackView.addArrangedSubview(titleView)
-        }
-
-        if let contentView = delegate?.popupContentView(popupVc: self) {
-            popupStackView.addArrangedSubview(contentView)
-        }
-
-        if let bottomView = delegate?.popupBottomView(popupVc: self) {
-            popupStackView.addArrangedSubview(bottomView)
-        }
-
-        view.addSubview(whiteBoard)
-        whiteBoard.addSubview(popupStackView)
+    override func addView() {
+        layoutView.addSubview(layerView)
     }
 
-    private func layout() {
-        whiteBoard.snp.makeConstraints {
+    override func layout() {
+        layerView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.left.right.equalToSuperview().inset(16)
-        }
-
-        popupStackView.snp.makeConstraints {
-            $0.left.right.equalToSuperview().inset(16)
-            $0.top.bottom.equalToSuperview().inset(24)
         }
     }
 
     @objc func dismissPopup() {
         dismiss(animated: true, completion: nil)
     }
-}
 
-extension TDPopupViewController: UIGestureRecognizerDelegate {
     func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
         return touch.view == gestureRecognizer.view
     }
