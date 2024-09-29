309a5638151f0193eb523b3626eec014ef697397
Kyxxn <gywns626@naver.com>
2024-09-29 18:31:34 +0900

    feat: Schedule 모델에 따른 날짜 아래 이벤트 지정
    Delegate와 Appearance 메소드를 사용하여
    날짜 아래에 점 개수와 모델로부터 받아온 색상 지정

--- toduck/Source/Presentation/Calendar/ToduckCalendarViewController.swift
+++ toduck/Source/Presentation/Calendar/ToduckCalendarViewController.swift
@@ -10,11 +10,50 @@ import SnapKit
 import Then
 import UIKit
 
+struct TempSchedule {
+    let name: String
+    let color: UIColor
+}
+
 final class ToduckCalendarViewController: BaseViewController<BaseView> {
     // MARK: - UI Components
     let calendarHeader = CalendarHeaderStackView(type: .toduck)
     let calendar = ToduckCalendar()
     private let selectedDayScheduleView = SelectedDayScheduleView()
+    
+    // MARK: - Properties
+    let tempSchedules: [Int: [TempSchedule]] = [
+        15: [
+            TempSchedule(name: "테스트1", color: .red),
+            TempSchedule(name: "테스트2", color: .blue),
+            TempSchedule(name: "테스트2", color: .blue),
+            TempSchedule(name: "테스트2", color: .blue)
+        ],
+        16: [
+            TempSchedule(name: "테스트3", color: .green),
+            TempSchedule(name: "테스트4", color: .yellow)
+        ],
+        17: [
+            TempSchedule(name: "테스트5", color: .purple),
+            TempSchedule(name: "테스트6", color: .orange)
+        ],
+        18: [
+            TempSchedule(name: "테스트7", color: .brown),
+            TempSchedule(name: "테스트8", color: .cyan)
+        ],
+        19: [
+            TempSchedule(name: "테스트9", color: .magenta),
+            TempSchedule(name: "테스트10", color: .systemPink)
+        ],
+        20: [
+            TempSchedule(name: "테스트11", color: .systemTeal),
+            TempSchedule(name: "테스트12", color: .systemIndigo)
+        ],
+        21: [
+            TempSchedule(name: "테스트13", color: .systemGray),
+            TempSchedule(name: "테스트14", color: .systemYellow)
+        ]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -59,6 +98,7 @@ extension ToduckCalendarViewController: TDCalendarConfigurable {
         updateHeaderLabel(for: calendar.currentPage)
     }
     
+    // MARK: - 날짜 폰트 색상
     // 기본 폰트 색
     func calendar(
         _ calendar: FSCalendar,
@@ -75,5 +115,42 @@ extension ToduckCalendarViewController: TDCalendarConfigurable {
         titleSelectionColorFor date: Date
     ) -> UIColor? {
         colorForDate(date)
+    }
+    
+    // MARK: - 날짜 아래의 이벤트
+    // 날짜 아래 점 개수 지정
+    func calendar(
+        _ calendar: FSCalendar,
+        numberOfEventsFor date: Date
+    ) -> Int {
+        let day = Calendar.current.component(.day, from: date)
+        guard let schedules = tempSchedules[day] else { return 0 }
+        return schedules.count
+    }
+    
+    // 날짜 아래 점 색상 지정 (이벤트 색상)
+    func calendar(
+        _ calendar: FSCalendar,
+        appearance: FSCalendarAppearance,
+        eventDefaultColorsFor date: Date
+    ) -> [UIColor]? {
+        colorFromEvent(for: date)
+    }
+    
+    // 선택된 날짜에도 동일한 이벤트 색상을 유지하도록 설정
+    func calendar(
+        _ calendar: FSCalendar,
+        appearance: FSCalendarAppearance,
+        eventSelectionColorsFor date: Date
+    ) -> [UIColor]? {
+        colorFromEvent(for: date)
+    }
+    
+    // TODO: 나중에 TDCalendarConfigurable로 옮겨야 함, tempSchedules 생각하기
+    // 날짜에 대한 일정 색상을 반환하는 헬퍼 메서드
+    func colorFromEvent(for date: Date) -> [UIColor]? {
+        let day = Calendar.current.component(.day, from: date)
+        guard let schedules = tempSchedules[day] else { return nil }
+        return schedules.map { $0.color }
     }
 }
