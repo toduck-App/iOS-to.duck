f0be97ce384df66e105eada770758a4886a4727b
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-10-24 21:59:05 +0900

    refactor: Public 빼먹은 부분 수정

--- Projects/TDDesign/Sources/Common/ActiveColor.swift
+++ Projects/TDDesign/Sources/Common/ActiveColor.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 public struct ActiveColor {
-    var activeColor: UIColor
-    var inActiveColor: UIColor
+    public let activeColor: UIColor
+    public let inActiveColor: UIColor
+    
+    public init(
+        activeColor: UIColor,
+        inActiveColor: UIColor
+    ) {
+        self.activeColor = activeColor
+        self.inActiveColor = inActiveColor
+    }
 }
