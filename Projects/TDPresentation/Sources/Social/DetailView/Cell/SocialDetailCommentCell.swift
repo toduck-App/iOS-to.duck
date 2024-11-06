f923cb09aa8cae1e82d9f909bd02f46d7ca7d09a
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2024-11-06 23:51:54 +0900

    feat: Comment Cell 생성

--- Projects/TDPresentation/Sources/Social/DetailView/Cell/SocialDetailCommentCell.swift
+++ Projects/TDPresentation/Sources/Social/DetailView/Cell/SocialDetailCommentCell.swift
@@ -10,6 +10,9 @@ protocol SocialDetailCommentCellDelegate: AnyObject {
 
 final class SocialDetailCommentCell: UICollectionViewCell {
     weak var commentDelegate: SocialDetailCommentCellDelegate?
+    
+    private let containerView = UIView()
+    
     private var verticalStackView = UIStackView().then {
         $0.axis = .vertical
         $0.spacing = 14
@@ -54,7 +57,23 @@ final class SocialDetailCommentCell: UICollectionViewCell {
         setupUI()
     }
     
-    func configure(with comment: Comment) {}
+    func configure(with item: Comment) {
+        headerView.configure(titleBadge: item.user.title, nickname: item.user.name, date: item.timestamp)
+        contentLabel.setText(item.content)
+        footerView.configure(isLike: item.isLike, likeCount: item.like, commentCount: nil, shareCount: nil)
+        configureUserImage(with: item.user.icon)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        contentLabel.setText("")
+        avatarView.image = TDImage.Profile.medium
+        bodyStackView.arrangedSubviews.forEach {
+            if $0 is SocialRoutineView || $0 is SocialImageListView {
+                $0.removeFromSuperview()
+            }
+        }
+    }
 }
 
 // MARK: Layout
@@ -66,11 +85,59 @@ private extension SocialDetailCommentCell {
     }
     
     func setupLayout() {
+        addSubview(containerView)
+        [avatarView, verticalStackView].forEach{
+            containerView.addSubview($0)
+        }
+        bodyStackView.addArrangedSubview(contentLabel)
         
+        [headerView, bodyStackView, footerView].forEach{
+            verticalStackView.addArrangedSubview($0)
+        }
     }
     
     func setupConstraints() {
+        containerView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(20)
+            make.bottom.equalToSuperview().offset(-20)
+            make.leading.trailing.equalToSuperview()
+        }
         
+        avatarView.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview()
+            make.size.equalTo(36)
+        }
+        
+        headerView.snp.makeConstraints { make in
+            make.height.equalTo(24)
+        }
+        
+        bodyStackView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+        }
+        
+        footerView.snp.makeConstraints { make in
+            make.height.equalTo(24)
+        }
+        
+        verticalStackView.snp.makeConstraints { make in
+            make.leading.equalTo(avatarView.snp.trailing).offset(10)
+            make.trailing.equalToSuperview().offset(-16)
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+}
+
+// MARK: Private Method
+
+private extension SocialDetailCommentCell {
+    func configureUserImage(with image: String?) {
+        if let image = image {
+            avatarView.kf.setImage(with: URL(string: image))
+        } else {
+            avatarView.image = TDImage.Profile.medium
+        }
     }
 }
 
