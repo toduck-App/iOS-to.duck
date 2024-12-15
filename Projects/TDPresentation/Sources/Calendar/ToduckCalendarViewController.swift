5a477b042ad547feb7f2c757a3d8ebeeb98b1afa
박효준 <gywns626@naver.com>
2024-12-16 02:58:03 +0900

    fix: GNB 캘린더 갔다오면 네비바 사라지는 문제해결

--- Projects/TDPresentation/Sources/Calendar/ToduckCalendarViewController.swift
+++ Projects/TDPresentation/Sources/Calendar/ToduckCalendarViewController.swift
@@ -90,7 +90,6 @@ final class ToduckCalendarViewController: BaseViewController<BaseView> {
     
     // MARK: - Setup
     private func setup() {
-        navigationController?.navigationBar.isHidden = true
         selectedDayScheduleView.scheduleTableView.delegate = self
         selectedDayScheduleView.scheduleTableView.dataSource = self
         selectedDayScheduleView.scheduleTableView.separatorInset = UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
@@ -119,8 +118,8 @@ final class ToduckCalendarViewController: BaseViewController<BaseView> {
     
     private func setupConstraints() {
         calendarHeader.snp.makeConstraints {
-            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(30)
-            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(15)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(4)
+            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
         }
         calendar.snp.makeConstraints {
             $0.centerX.equalTo(view)
