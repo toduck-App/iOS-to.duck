9b478c03d7693d040d054012e67e4215a916b594
박효준 <gywns626@naver.com>
2024-12-15 21:27:16 +0900

    chore: 린트 정리

--- Projects/toduck/Sources/SceneDelegate.swift
+++ Projects/toduck/Sources/SceneDelegate.swift
@@ -20,8 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        assembleDependencies()
+        
         let injector: DependencyResolvable = DIContainer.shared
+        assembleDependencies()
         appCoordinator = AppCoordinator(navigationController: navigationController, injector: injector)
         appCoordinator?.start()
         
