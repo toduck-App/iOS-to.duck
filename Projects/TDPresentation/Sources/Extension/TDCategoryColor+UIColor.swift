a4b412d25e8f1af33002d960d5e6af3ebf949268
Kyxxn <gywns626@naver.com>
2024-12-23 03:07:30 +0900

    feat: Present Layer에서 Domain의 TDCategoryColor 색상 지정

---
+++ Projects/TDPresentation/Sources/Extension/TDCategoryColor+UIColor.swift
@@ -0,0 +1,24 @@
+import UIKit
+import TDDesign
+import TDDomain
+
+extension TDCategoryColor {
+    public var color: UIColor {
+        switch self {
+        case .back1: return TDColor.Schedule.back1
+        case .back2: return TDColor.Schedule.back2
+        case .back3: return TDColor.Schedule.back3
+        case .back4: return TDColor.Schedule.back4
+        case .back5: return TDColor.Schedule.back5
+        case .back6: return TDColor.Schedule.back6
+        case .back7: return TDColor.Schedule.back7
+        case .back8: return TDColor.Schedule.back8
+        case .back9: return TDColor.Schedule.back9
+        case .back10: return TDColor.Schedule.back10
+        case .back11: return TDColor.Schedule.back11
+        case .back12: return TDColor.Schedule.back12
+        case .back13: return TDColor.Schedule.back13
+        case .back14: return TDColor.Schedule.back14
+        }
+    }
+}
