4c14fd2ddcd69379b425e0935840d40f21c0dcd5
SeungJae Son <46300191+Sonny-Kor@users.noreply.github.com>
2025-01-11 01:54:33 +0900

    feat: ViewModel 검색어 추천 기능 Update
    키워드 관련 업데이트

--- Projects/TDPresentation/Sources/Social/Search/SocialSearchViewModel.swift
+++ Projects/TDPresentation/Sources/Social/Search/SocialSearchViewModel.swift
@@ -3,11 +3,15 @@ import TDDomain
 
 final class SocialSearchViewModel: BaseViewModel {
     enum Input {
+        case loadInitialData
+        case deleteRecentKeyword(index: Int)
         case search(query: String)
         case selectPost(post: Post.ID)
     }
     
     enum Output {
+        case updateRecentKeywords
+        case updatePopularKeywords
         case searchResult
         case selectPost
         case failure(String)
@@ -16,6 +20,8 @@ final class SocialSearchViewModel: BaseViewModel {
     private let searchPostUseCase: SearchPostUseCase
     private let output = PassthroughSubject<Output, Never>()
     private var cancellables: Set<AnyCancellable> = []
+    private(set) var recentKeywords: [String] = []
+    private(set) var popularKeywords: [String] = []
     private(set) var searchResult: [Post] = []
     private(set) var selectedPostID: Post.ID?
     
@@ -27,10 +33,14 @@ final class SocialSearchViewModel: BaseViewModel {
         input.sink { [weak self] event in
             guard let self else { return }
             switch event {
+            case .loadInitialData:
+                Task { await self.loadKeywords() }
+            case .deleteRecentKeyword(index: let index):
+                deleteRecentKeyword(index: index)
             case .search(let term):
                 Task { await self.searchPost(term: term) }
             case .selectPost(let post):
-                self.selectPost(postID: post)
+                selectPost(postID: post)
             }
         }
         .store(in: &cancellables)
@@ -40,9 +50,11 @@ final class SocialSearchViewModel: BaseViewModel {
     
     func searchPost(term: String) async {
         do {
+            saveKeywords(term: term)
             let posts = try await searchPostUseCase.execute(keyword: term)
             searchResult = posts ?? []
             output.send(.searchResult)
+            
         } catch {
             output.send(.failure(error.localizedDescription))
         }
@@ -51,5 +63,23 @@ final class SocialSearchViewModel: BaseViewModel {
     func selectPost(postID: Post.ID) {
         selectedPostID = postID
         output.send(.selectPost)
+    }
+    
+    private func loadKeywords() async {
+        recentKeywords = ["콘서타", "루틴", "불면증"]
+        popularKeywords = ["루틴", "집중", "뽀모도로", "꿀팁", "시간관리", "ADHD", "일기"]
+        output.send(.updateRecentKeywords)
+        output.send(.updatePopularKeywords)
+    }
+    
+    private func deleteRecentKeyword(index: Int) {
+        recentKeywords.remove(at: index)
+        output.send(.updateRecentKeywords)
+    }
+    
+    private func saveKeywords(term: String) {
+        // UserDefaults에 저장, 중복 검사와 최대 치를 넘어가면 최근 검색어 삭제하는 로직이 Repository나 UseCase에서 처리되어야 할 것 같음.
+        self.recentKeywords.append(term)
+        output.send(.updateRecentKeywords)
     }
 }
