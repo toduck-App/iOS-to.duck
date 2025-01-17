e5fac9af94f764d877cfcea1b748643508544c24
Kyxxn <gywns626@naver.com>
2025-01-17 20:06:00 +0900

    feat: Tuist 설정 파일에서 테스트 모듈들에 의존성 부여

--- Projects/TDStorage/Project.swift
+++ Projects/TDStorage/Project.swift
@@ -28,6 +28,10 @@ let project = Project.project(
             bundleId: Project.bundleID + ".storagetest",
             sources: .tests,
             dependencies: [
+                .storage(),
+                .core(),
+                .domain(),
+                .data()
             ]
         )
     ]
