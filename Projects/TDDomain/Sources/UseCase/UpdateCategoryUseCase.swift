26a783f66bf7fae6fd49c0ad09611a69dfb00d1b
Kyxxn <gywns626@naver.com>
2025-01-17 18:37:48 +0900

    feat: 카테고리들 UserDefaults에 색상 업데이트 UseCase 구현

---
+++ Projects/TDDomain/Sources/UseCase/UpdateCategoryUseCase.swift
@@ -0,0 +1,17 @@
+import TDCore
+
+public protocol UpdateCategoriesUseCase {
+    func execute(categories: [TDCategory]) async throws
+}
+
+public final class UpdateCategoriesUseCaseImpl: UpdateCategoriesUseCase {
+    private let repository: CategoryRepository
+    
+    public init(repository: CategoryRepository) {
+        self.repository = repository
+    }
+    
+    public func execute(categories: [TDCategory]) async throws {
+        try await repository.updateCategories(categories: categories).get()
+    }
+}
