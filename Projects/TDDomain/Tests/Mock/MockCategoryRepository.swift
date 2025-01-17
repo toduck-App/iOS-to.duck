f9dc92ba771a9f1fc3eac6969f64219fb2537462
박효준 <gywns626@naver.com>
2025-01-18 00:32:06 +0900

    feat: Domain 레이어에서 카테고리 Update 테스트 구현

--- Projects/TDDomain/Tests/Mock/MockCategoryRepository.swift
+++ Projects/TDDomain/Tests/Mock/MockCategoryRepository.swift
@@ -5,6 +5,12 @@ final class MockCategoryRepository: CategoryRepository {
     var stubbedFetchCategoriesResult: [TDCategory] = []
     var stubbedFetchCategoriesError: TDDataError?
 
+    var stubbedUpdateCategoriesResult: Result<Void, TDDataError> = .success(())
+    var stubbedUpdateCategoriesError: TDDataError?
+
+    var invokedUpdateCategories = false
+    var invokedUpdateCategoriesParameters: [TDCategory]?
+
     func fetchCategories() async throws -> [TDCategory] {
         if let error = stubbedFetchCategoriesError {
             throw error
@@ -13,6 +19,12 @@ final class MockCategoryRepository: CategoryRepository {
     }
 
     func updateCategories(categories: [TDCategory]) async throws -> Result<Void, TDDataError> {
-        return .success(())
+        invokedUpdateCategories = true
+        invokedUpdateCategoriesParameters = categories
+
+        if let error = stubbedUpdateCategoriesError {
+            throw error
+        }
+        return stubbedUpdateCategoriesResult
     }
 }
