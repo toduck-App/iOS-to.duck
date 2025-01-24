2fd40f786d2c682a234e3c2b93e639a83e41f5e3
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2025-01-24 13:25:44 +0900

    feat: Timer Entity추가

---
+++ Projects/TDDomain/Sources/Entity/Timer.swift
@@ -0,0 +1,32 @@
+//
+//  Timer.swift
+//  TDDomain
+//
+//  Created by 신효성 on 12/30/24.
+//
+
+public struct TDTimerSetting {
+    public var focusDuration: Int
+    public var foucsCount: Int
+    public var restDuration: Int
+        
+    public init(focusDuration: Int, foucsCount: Int, restDuration: Int) {
+        self.focusDuration = focusDuration
+        self.foucsCount = foucsCount
+        self.restDuration = restDuration
+    }
+    
+    public func toMiniutes() -> Int {
+        return focusDuration * 60
+    }
+}
+
+
+
+//MARK: - Dummy Extension
+extension TDTimerSetting {
+    public static func dummy() -> TDTimerSetting {
+        return TDTimerSetting(focusDuration: 30,foucsCount: 1,restDuration: 10)
+    }
+}
+
