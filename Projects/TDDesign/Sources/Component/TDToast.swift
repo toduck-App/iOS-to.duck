101ed0a7e2a994716520a6f193e557ca6ba3da53
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2025-01-24 13:32:55 +0900

    refactor: Toast

--- Projects/TDDesign/Sources/Component/TDToast.swift
+++ Projects/TDDesign/Sources/Component/TDToast.swift
@@ -137,3 +137,27 @@ final class TDToastView: UIView {
 		stackY.addArrangedSubview(content)
 	}
 }
+
+extension TDToast {
+	public enum TDToastType {
+		case orange
+		case green
+
+		var color: UIColor {
+			switch self {
+			case .green:
+				return TDColor.Semantic.success
+			case .orange:
+				return TDColor.Primary.primary500
+			}
+		}
+		var tomato: UIImage {
+			switch self {
+			case .green:
+				return TDImage.Tomato.green
+			case .orange:
+				return TDImage.Tomato.orange
+			}
+		}
+	}
+}
--- Projects/TDPresentation/Sources/Protocol/TDToastPresentable.swift
+++ Projects/TDPresentation/Sources/Protocol/TDToastPresentable.swift
@@ -1,44 +1,39 @@
-//
-//  TDToastPresentation.swift
-//  TDPresentation
-//
-//  Created by 신효성 on 1/1/25.
-//
+import SnapKit
 import TDDesign
 import UIKit
-import SnapKit
 
 protocol TDToastPresentable {
-	func showToast(type: TDToastType, title: String, message: String)
+    func showToast(type: TDToast.TDToastType, title: String, message: String)
 }
 
 extension TDToastPresentable where Self: UIViewController {
-	func showToast(type: TDToastType, title: String, message: String) {
+	func showToast(type: TDToast.TDToastType, title: String, message: String) {
 		let toastView = TDToast(toastType: type, titleText: title, contentText: message)
 
-        guard let keyWindow = UIApplication.shared.connectedScenes
-                .compactMap({ $0 as? UIWindowScene })
-                .flatMap({ $0.windows })
-                .first(where: { $0.isKeyWindow }) else {
-            return
-        }
+		guard
+			let keyWindow = UIApplication.shared.connectedScenes
+				.compactMap({ $0 as? UIWindowScene })
+				.flatMap({ $0.windows })
+				.first(where: { $0.isKeyWindow })
+		else {
+			return
+		}
 
-        
-        keyWindow.addSubview(toastView)
-        toastView.snp.makeConstraints { make in
-            make.top.equalTo(view.safeAreaLayoutGuide).inset(-69)
-            make.leading.trailing.equalToSuperview().inset(20)
-        }
-        keyWindow.layoutIfNeeded()
+		keyWindow.addSubview(toastView)
+		toastView.snp.makeConstraints { make in
+			make.top.equalTo(view.safeAreaLayoutGuide).inset(-69)
+			make.leading.trailing.equalToSuperview().inset(20)
+		}
+		keyWindow.layoutIfNeeded()
 
 		// 애니메이션으로 화면 아래로 이동하여 표시
 		UIView.animate(
-            withDuration: 0.5, delay: 0, options: .curveEaseInOut,
+			withDuration: 0.5, delay: 0, options: .curveEaseInOut,
 			animations: {
-                toastView.snp.updateConstraints { make in
-                    make.top.equalTo(self.view.safeAreaLayoutGuide).inset(10)
-                }
-                keyWindow.layoutIfNeeded()
+				toastView.snp.updateConstraints { make in
+					make.top.equalTo(self.view.safeAreaLayoutGuide).inset(10)
+				}
+				keyWindow.layoutIfNeeded()
 			}
 		) { _ in
 			// 2초 대기 후 페이드 아웃 및 제거
