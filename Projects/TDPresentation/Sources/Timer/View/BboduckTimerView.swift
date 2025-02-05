c49e624b3637671a2157aeed934c538b0513ecd9
N-Joy-Shadow <57449485+N-Joy-Shadow@users.noreply.github.com>
2025-02-05 20:13:20 +0900

    refactor: 들여쓰기

--- Projects/TDPresentation/Sources/Timer/View/BboduckTimerView.swift
+++ Projects/TDPresentation/Sources/Timer/View/BboduckTimerView.swift
@@ -3,33 +3,30 @@ import TDDesign
 import UIKit
 
 final class BboduckTimerView: UIView {
-	let backdropView = UIView().then {
-		$0.backgroundColor = UIColor(red: 1.00, green: 0.75, blue: 0.58, alpha: 1.00)
-	}
+    let backdropView = UIView().then {
+        $0.backgroundColor = UIColor(red: 1.00, green: 0.75, blue: 0.58, alpha: 1.00)
+    }
 
-	let BboduckView = UIImageView().then {
-		$0.image = TDImage.Tomato.tomato
-		$0.contentMode = .scaleAspectFit
-	}
+    let BboduckView = UIImageView().then {
+        $0.image = TDImage.Tomato.tomato
+        $0.contentMode = .scaleAspectFit
+    }
 
-	override init(frame: CGRect = .zero) {
-		super.init(frame: frame)
-		addViews()
-		layout()
-	}
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        addViews()
+        layout()
+    }
 
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-		addViews()
-		layout()
-	}
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        addViews()
+        layout()
+    }
 
-	func addViews() {
-		addSubview(backdropView)
+    func addViews() {
+        addSubview(backdropView)
+    }
 
-	}
-
-	func layout() {
-		
-	}
+    func layout() {}
 }
