f0f7668aba8fc58a0deeda203cab0429276e19ed
박효준 <gywns626@naver.com>
2025-02-11 19:03:28 +0900

    chore: 더미데이터 중복코드 제거

--- Projects/TDDomain/Sources/Entity/Schedule.swift
+++ Projects/TDDomain/Sources/Entity/Schedule.swift
@@ -42,3 +42,35 @@ public struct Schedule: Eventable {
         self.isFinished = isFinished
     }
 }
+
+extension Schedule {
+    public static let dummy: [Schedule] = [
+        Schedule(
+            id: 0,
+            title: "1번 일정",
+            category: TDCategory(colorHex: "#123456", imageName: "power"),
+            startDate: "2021-09-01",
+            endDate: "2022-10-10",
+            isAllDay: false,
+            time: nil,
+            repeatDays: nil,
+            alarmTimes: nil,
+            place: "장소1",
+            memo: "메모1",
+            isFinished: false
+        ), Schedule(
+            id: 0,
+            title: "2번 일정",
+            category: TDCategory(colorHex: "#FFE3CC", imageName: "sleep"),
+            startDate: "2021-09-01",
+            endDate: "2022-10-10",
+            isAllDay: false,
+            time: Date(),
+            repeatDays: nil,
+            alarmTimes: nil,
+            place: "장소2",
+            memo: "메모2",
+            isFinished: false
+        )
+    ]
+}
--- Projects/TDPresentation/Sources/Home/ScheduleAndRoutine/ViewModel/ScheduleViewModel.swift
+++ Projects/TDPresentation/Sources/Home/ScheduleAndRoutine/ViewModel/ScheduleViewModel.swift
@@ -32,62 +32,10 @@ extension ScheduleViewModel: TimeSlotProvider {
     var timeSlots: [TimeSlot] {
         return [TimeSlot(
             timeText: "10:00",
-            events: [Schedule(
-                id: 0,
-                title: "1번 일정",
-                category: TDCategory(colorHex: "#123456", imageName: "power"),
-                startDate: "2021-09-01",
-                endDate: "2022-10-10",
-                isAllDay: false,
-                time: nil,
-                repeatDays: nil,
-                alarmTimes: nil,
-                place: "장소1",
-                memo: "메모1",
-                isFinished: false
-            ), Schedule(
-                id: 0,
-                title: "2번 일정",
-                category: TDCategory(colorHex: "#FFE3CC", imageName: "sleep"),
-                startDate: "2021-09-01",
-                endDate: "2022-10-10",
-                isAllDay: false,
-                time: Date(),
-                repeatDays: nil,
-                alarmTimes: nil,
-                place: "장소2",
-                memo: "메모2",
-                isFinished: false
-            )]
+            events: Schedule.dummy
         ), TimeSlot(
             timeText: "12:00",
-            events: [Schedule(
-                id: 0,
-                title: "3번 일정",
-                category: TDCategory(colorHex: "#555555", imageName: "power"),
-                startDate: "2021-09-01",
-                endDate: "2022-10-10",
-                isAllDay: false,
-                time: nil,
-                repeatDays: nil,
-                alarmTimes: nil,
-                place: "장소1",
-                memo: "메모1",
-                isFinished: false
-            ), Schedule(
-                id: 0,
-                title: "4번 일정",
-                category: TDCategory(colorHex: "#444444", imageName: "sleep"),
-                startDate: "2021-09-01",
-                endDate: "2022-10-10",
-                isAllDay: false,
-                time: nil,
-                repeatDays: nil,
-                alarmTimes: nil,
-                place: "장소2",
-                memo: "메모2",
-                isFinished: true
-            )]
+            events: Schedule.dummy
         )]
     }
 }
