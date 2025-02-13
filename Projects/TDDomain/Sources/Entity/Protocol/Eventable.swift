43247cdbfa46b2fa4f45cd74e0085020279f9c9b
박효준 <gywns626@naver.com>
2025-02-13 21:56:14 +0900

    chore: 프로퍼티 순서 정렬

--- Projects/TDDomain/Sources/Entity/Protocol/Eventable.swift
+++ Projects/TDDomain/Sources/Entity/Protocol/Eventable.swift
@@ -3,9 +3,9 @@ import Foundation
 public protocol Eventable {
     var id: Int? { get }
     var title: String { get }
-    var memo: String? { get} 
     var category: TDCategory { get }
     var time: Date? { get }
+    var memo: String? { get }
     var isFinished: Bool { get }
     var isRepeating: Bool { get }
 }
