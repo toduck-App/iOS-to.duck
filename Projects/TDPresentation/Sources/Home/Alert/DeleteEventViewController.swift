5c09f88a541846ce50df69686f1a24140b348bc3
박효준 <gywns626@naver.com>
2025-02-14 20:23:28 +0900

    feat: 이벤트 셀의 우측 스와이프 삭제 버튼 UI 구현

---
+++ Projects/TDPresentation/Sources/Home/Alert/DeleteEventViewController.swift
@@ -0,0 +1,9 @@
+import TDCore
+import TDDesign
+import UIKit
+
+final class DeleteEventViewController: TDPopupViewController<DeleteEventView> {
+    override func configure() {
+        super.configure()
+    }
+}
