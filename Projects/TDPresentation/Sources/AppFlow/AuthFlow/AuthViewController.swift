371deda9599699fd3664af6c886a8c227fb79931
박효준 <gywns626@naver.com>
2025-02-26 21:08:35 +0900

    feat: Auth 화면 (Oauth와 자체 회원가입) UI 구현

--- Projects/TDPresentation/Sources/AppFlow/AuthFlow/AuthViewController.swift
+++ Projects/TDPresentation/Sources/AppFlow/AuthFlow/AuthViewController.swift
@@ -6,6 +6,18 @@ import TDDesign
 final class AuthViewController: BaseViewController<AuthView> {
     weak var coordinator: AuthCoordinator?
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.isNavigationBarHidden = false
+    }
+    
     override func configure() {
         layoutView.mainButton.addAction(UIAction { [weak self] _ in
             self?.coordinator?.didMainButtonTapped()
