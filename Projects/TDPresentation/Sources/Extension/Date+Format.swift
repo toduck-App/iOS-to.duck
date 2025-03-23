ceb50c624ddbf9fdca94e765b366a093c1466008
박효준 <gywns626@naver.com>
2025-03-23 21:15:56 +0900

    feat: Date 타입 유연성을 위한 normalized 연산 프로퍼티 추가

--- Projects/TDPresentation/Sources/Extension/Date+Format.swift
+++ Projects/TDPresentation/Sources/Extension/Date+Format.swift
@@ -5,6 +5,6 @@ extension Date {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
         formatter.dateFormat = "M월 d일 (E)" // "3월 26일 (화)" 형식
-        return formatter.string(from: Date())
+        return formatter.string(from: self)
     }
 }
