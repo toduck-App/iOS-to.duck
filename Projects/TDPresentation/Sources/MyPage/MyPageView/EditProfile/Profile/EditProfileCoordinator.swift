58567e574d84b7b958bfd90d090fc1b43dcd2ad2
Jiyong <hickerd@sch.ac.kr>
2025-03-27 22:00:51 +0900

    feat: #94 - member profile modification

---
+++ Projects/TDPresentation/Sources/MyPage/MyPageView/EditProfile/Profile/EditProfileCoordinator.swift
@@ -0,0 +1,37 @@
+//
+//  EditProfileCoordinator.swift
+//  TDPresentation
+//
+//  Created by 정지용 on 1/23/25.
+//
+
+import UIKit
+
+import TDCore
+
+final class EditProfileCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    var childCoordinators = [Coordinator]()
+    var finishDelegate: CoordinatorFinishDelegate?
+    var injector: DependencyResolvable
+    
+    init(
+        navigationController: UINavigationController,
+        injector: DependencyResolvable
+    ) {
+        self.navigationController = navigationController
+        self.injector = injector
+    }
+    
+    func start() {
+        let editProfileViewController = EditProfileViewController()
+        editProfileViewController.hidesBottomBarWhenPushed = true
+        editProfileViewController.coordinator = self
+        navigationController.pushViewController(editProfileViewController, animated: true)
+    }
+    
+    func popViewController() {
+        navigationController.popViewController(animated: true)
+        finishDelegate?.didFinish(childCoordinator: self)
+    }
+}
