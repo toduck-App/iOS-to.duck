1786b481bdb08e9ae49d13eaeb48b3785006401a
박효준 <gywns626@naver.com>
2025-05-10 23:22:28 +0900

    feat: 토덕 딥링크 타입 정의

---
+++ Projects/TDCore/Sources/DeepLinkType.swift
@@ -0,0 +1,44 @@
+import Foundation
+
+public enum DeepLinkType {
+    case profile(userId: String)
+    case post(postId: String, commentId: String?)
+    case todo
+    case focus
+    case diary
+    case home
+    case notification
+    
+    public init?(url: URL) {
+        guard url.scheme == "toduck" else { return nil }
+        
+        switch url.host {
+        case "profile":
+            guard let userId = url.queryParameters?["userId"] else { return nil }
+            self = .profile(userId: userId)
+            
+        case "post":
+            guard let postId = url.queryParameters?["postId"] else { return nil }
+            let commentId = url.queryParameters?["commentId"]
+            self = .post(postId: postId, commentId: commentId)
+            
+        case "todo":
+            self = .todo
+            
+        case "focus":
+            self = .focus
+            
+        case "diary":
+            self = .diary
+            
+        case "home":
+            self = .home
+            
+        case "notification":
+            self = .notification
+            
+        default:
+            return nil
+        }
+    }
+}
