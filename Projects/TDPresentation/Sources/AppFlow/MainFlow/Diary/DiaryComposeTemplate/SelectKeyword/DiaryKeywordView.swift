83d021c257007f027a79ee28df07d2bdb7b7bb91
박효준 <gywns626@naver.com>
2025-10-19 14:16:21 +0900

    feat: 일기 키워드 작성 화면 UI 구현

--- Projects/TDPresentation/Sources/AppFlow/MainFlow/Diary/DiaryComposeTemplate/SelectKeyword/DiaryKeywordView.swift
+++ Projects/TDPresentation/Sources/AppFlow/MainFlow/Diary/DiaryComposeTemplate/SelectKeyword/DiaryKeywordView.swift
@@ -4,35 +4,123 @@ import TDDesign
 final class DiaryKeywordView: BaseView {
     // MARK: - UI Components
     
+    let scrollView = UIScrollView()
+    let stackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 0
+    }
+    
+    let labelStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 8
+        $0.alignment = .center
+    }
     let titleLabel = TDLabel(
-        labelText: "오늘의 기분은 어땠나요?",
+        labelText: "하루를 키워드로 정리해 볼까요?",
         toduckFont: .boldHeader4,
         toduckColor: TDColor.Neutral.neutral800
     )
     let descriptionLabel = TDLabel(
-        labelText: "가장 가까운 감정 이모지를 선택해주세요",
+        labelText: "책을 꾹 누르면 선택 키워드가 초기화 돼요",
         toduckFont: .boldBody2,
         toduckColor: TDColor.Neutral.neutral600
+    )
+    
+    let keywordContentStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 0
+        $0.alignment = .center
+    }
+    let currentBookImageView = UIImageView(image: TDImage.BookKeyword.none)
+    let noSelectedLabel = TDLabel(
+        labelText: "키워드를 선택해 주세요",
+        toduckFont: .boldBody1,
+        toduckColor: TDColor.Neutral.neutral600
+    )
+    
+    let keywordCategorySegment = TDSegmentedControl(items: ["전체", "사람", "장소", "상황", "결과/느낌"])
+    
+    let buttonContainerView = UIView().then {
+        $0.backgroundColor = TDColor.baseWhite
+    }
+    let buttonStackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.backgroundColor = TDColor.baseWhite
+        $0.spacing = 10
+    }
+    let skipButton = TDBaseButton(
+        title: "건너뛰기",
+        backgroundColor: TDColor.baseWhite,
+        foregroundColor: TDColor.Neutral.neutral700,
+        radius: 12,
+        font: TDFont.boldHeader3.font
+    )
+    let saveButton = TDBaseButton(
+        title: "저장",
+        backgroundColor: TDColor.Primary.primary500,
+        foregroundColor: TDColor.baseWhite,
+        radius: 12,
+        font: TDFont.boldHeader3.font
     )
     
     // MARK: - Common Methods
     
     override func addview() {
-        [
-            titleLabel,
-            descriptionLabel
-        ].forEach { addSubview($0) }
+        addSubview(scrollView)
+        addSubview(buttonContainerView)
+        scrollView.addSubview(stackView)
+        
+        stackView.addArrangedSubview(labelStackView)
+        labelStackView.addArrangedSubview(titleLabel)
+        labelStackView.addArrangedSubview(descriptionLabel)
+        
+        stackView.addArrangedSubview(keywordContentStackView)
+        keywordContentStackView.addArrangedSubview(currentBookImageView)
+        keywordContentStackView.addArrangedSubview(noSelectedLabel)
+        
+        stackView.addArrangedSubview(keywordCategorySegment)
+        
+        buttonContainerView.addSubview(buttonStackView)
+        buttonStackView.addArrangedSubview(skipButton)
+        buttonStackView.addArrangedSubview(saveButton)
     }
     
     override func layout() {
-        titleLabel.snp.makeConstraints {
-            $0.top.equalTo(safeAreaLayoutGuide).offset(42)
-            $0.centerX.equalToSuperview()
+        scrollView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(buttonContainerView.snp.top)
+        }
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(scrollView.contentLayoutGuide).offset(40)
+            $0.leading.trailing.bottom.equalTo(scrollView.contentLayoutGuide)
+            $0.width.equalTo(scrollView.frameLayoutGuide)
         }
         
-        descriptionLabel.snp.makeConstraints {
-            $0.top.equalTo(titleLabel.snp.bottom).offset(8)
-            $0.centerX.equalToSuperview()
+        currentBookImageView.snp.makeConstraints {
+            $0.size.equalTo(240)
         }
+
+        stackView.setCustomSpacing(60, after: keywordContentStackView)
+        
+        keywordCategorySegment.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(44)
+        }
+        
+        buttonContainerView.snp.makeConstraints {
+            $0.bottom.leading.trailing.equalToSuperview()
+            $0.height.equalTo(112)
+        }
+        buttonStackView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(16)
+            $0.height.equalTo(56)
+        }
+    }
+    
+    override func configure() {
+        backgroundColor = TDColor.baseWhite
+        skipButton.layer.borderWidth = 1
+        skipButton.layer.borderColor = TDColor.Neutral.neutral300.cgColor
     }
 }
