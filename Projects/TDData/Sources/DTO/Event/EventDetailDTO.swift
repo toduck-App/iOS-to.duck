57a641ab033cd881888f19689969740d0981722b
Son SeungJae <aldladdl3@naver.com>
2026-01-24 16:55:08 +0900

    fix: Event DTO 및 Domain 수정

--- Projects/TDData/Sources/DTO/Event/EventDetailDTO.swift
+++ Projects/TDData/Sources/DTO/Event/EventDetailDTO.swift
@@ -9,13 +9,17 @@ public struct EventDetailDTO: Decodable {
     public let eventsDetailId: Int
     public let eventsId: Int
     public let routingUrl: String
+    public let buttonVisible: Bool
+    public let buttonText: String
     public let eventsDetailImgUrl: [String]?
-    
+
     func convertToEventDetail() -> TDEventDetail {
         TDEventDetail(
             eventsDetailId: eventsDetailId,
             eventsId: eventsId,
             routingURL: URL(string: routingUrl),
+            isButtonVisible: buttonVisible,
+            buttonText: buttonText,
             imageURLs: eventsDetailImgUrl?.compactMap { URL(string: $0) } ?? []
         )
     }
--- Projects/TDDomain/Sources/Entity/Event/TDEventDetail.swift
+++ Projects/TDDomain/Sources/Entity/Event/TDEventDetail.swift
@@ -5,12 +5,23 @@ public struct TDEventDetail: Identifiable, Equatable {
     public let eventsDetailId: Int
     public let eventsId: Int
     public let routingURL: URL?
+    public let isButtonVisible: Bool
+    public let buttonText: String?
     public let imageURLs: [URL]
-    
-    public init(eventsDetailId: Int, eventsId: Int, routingURL: URL?, imageURLs: [URL]) {
+
+    public init(
+        eventsDetailId: Int,
+        eventsId: Int,
+        routingURL: URL?,
+        isButtonVisible: Bool,
+        buttonText: String?,
+        imageURLs: [URL]
+    ) {
         self.eventsDetailId = eventsDetailId
         self.eventsId = eventsId
         self.routingURL = routingURL
+        self.isButtonVisible = isButtonVisible
+        self.buttonText = buttonText
         self.imageURLs = imageURLs
     }
 }
